cb2ea4c19d4ffe6be554440ec84beb9c5a9b3a6f
A1tBot <timofeyershovv@gmail.com>
2024-07-17 00:57:27 +0700

    Новая JS функция для фильтрации

---
+++ static/js/n_filter.js
@@ -0,0 +1,59 @@
+var filterData = {};
+
+/*Применить фильтрацию*/
+$(document).ready(function() {
+    $('#submit-filter').click(function(){
+        filterData = {
+            'filterType': 0,
+            'personType': $('#person-type').val(),
+            'personAgeFrom': $('#age-range-from').val(),
+            'personAgeTo': $('#age-range-to').val(),
+            'languages': $('.language-checkbox:checked').map(function() {
+                return $(this).attr('id')
+            }).get()
+        }
+        console.log(filterData);
+        handleFilterData(filterData);
+    });
+})
+
+
+/*Сбросить фильтрацию*/
+$(document).ready(function() {
+    $('#reset-filter').click(function(){
+        $('#person-type').val('empty');
+        $('#age-range-from').val(1);
+        $('#age-range-to').val(99);
+        $('.language-checkbox').each(function() {
+            $(this).prop('checked',false);
+        });
+        filterData = {
+            'filterType': 1,
+            'personType': $('#person-type').val(),
+            'personAgeFrom': $('#age-range-from').val(),
+            'personAgeTo': $('#age-range-to').val(),
+            'languages': $('.language-checkbox:checked').map(function() {
+                return $(this).attr('id')
+            }).get()
+        }
+        console.log(filterData);
+        handleFilterData(filterData);
+    });
+})
+
+
+/*Отправка запроса на бэк и получение новых данных для таблицы и дальнейшего ее отображения*/
+function handleFilterData(filterData) {
+    $('#all-user-checkbox, .user-checkbox').prop('checked', false);
+    $.ajax({
+        url: '/filter_data',
+        type: 'POST',
+        data: JSON.stringify(filterData),
+        contentType: 'application/json',
+        success: function(htmlData) {
+            var table = $('.reset-table');
+            table.empty();
+            $('.reset-table').html(htmlData);
+        }
+    })
+}

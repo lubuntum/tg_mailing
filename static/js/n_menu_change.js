1bf64e955802e12a7d333bcc4ee4cf29a8a0a971
A1tBot <timofeyershovv@gmail.com>
2024-07-17 16:22:21 +0700

    Теперь меню не закрывается от нажатия в любую область экрана

--- static/js/n_menu_change.js
+++ static/js/n_menu_change.js
@@ -1,8 +1,5 @@
 var filter = document.getElementById('select-filter');
 var send = document.getElementById('select-send');
-var filterInput = document.getElementById('filter-input');
-var sendInput = document.getElementById('send-input');
-var fileName = document.getElementById('file-name');
 
 filter.addEventListener('change', function() {
     if (this.checked) {
@@ -19,28 +16,4 @@ send.addEventListener('change', function() {
 window.addEventListener('scroll', function() {
     filter.checked = false;
     send.checked = false;
-});
-
-filter.addEventListener('change', function() {
-    if (this.checked) {
-        function clickHandler(event) {
-            if (!filterInput.contains(event.target)) {
-                filter.checked = false;
-                document.removeEventListener('click', clickHandler);
-            }
-        };
-        document.addEventListener('click', clickHandler);
-    }
-});
-
-send.addEventListener('change', function() {
-    if (this.checked) {
-        function clickHandler(event) {
-            if (!sendInput.contains(event.target)) {
-                send.checked = false;
-                document.removeEventListener('click', clickHandler);
-            }
-        };
-        document.addEventListener('click', clickHandler);
-    }
 });

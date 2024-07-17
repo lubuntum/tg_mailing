5bc414f105d3935629664aecc0069318432c4e20
A1tBot <timofeyershovv@gmail.com>
2024-07-17 12:49:51 +0700

    Доделаны чекбоксы пользователей

--- static/js/n_user_ids.js
+++ static/js/n_user_ids.js
@@ -1,10 +1,39 @@
+var ids = []
+
 $(document).ready(function() {
-    $('#all-user-checkbox').change(function(){
-        if(this.checked) {
-            $('.user-checkbox').prop('checked', true);
+    $('#all-user-checkbox').on('click', function() {
+        if (this.checked) {
+            ids = []
+            $('.user-checkbox').each(function() {
+                $('.user-checkbox').prop('checked', true)
+                idToPush = $(this).closest('tr').find('td:eq(1)').text()
+                ids.push(idToPush)
+            })
+            console.log(ids)
         } else {
-            $('.user-checkbox').prop('checked', false);
+            $('.user-checkbox').each(function() {
+                $('.user-checkbox').prop('checked', false)
+                ids = []
+            })
+            console.log(ids)
         }
-    });
-    console.log(true);
+    })
+})
+
+$(document).ready(function() {
+    $('.user-checkbox').on('click', function() {
+        if (this.checked) {
+            $(this).prop('checked', true)
+            idToPush = $(this).closest('tr').find('td:eq(1)').text()
+            ids.push(idToPush)
+            console.log(ids)
+        } else {
+            $(this).prop('checked', false)
+            indexToDelete = ids.indexOf($(this).closest('tr').find('td:eq(1)').text())
+            if (indexToDelete > -1) {
+                ids.splice(indexToDelete, 1)
+            }
+            console.log(ids)
+        }
+    })
 })

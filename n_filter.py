482c93eaf0159237bef4ea79ad8d7cb8249c879c
A1tBot <timofeyershovv@gmail.com>
2024-07-19 12:46:04 +0700

    Поправили фильтрацию по зыкам

--- n_filter.py
+++ n_filter.py
@@ -27,8 +27,11 @@ def filterFunc(users, filter):
     #Сортировка по языкам
     languagesDict = {'english': 'Английский', 'german': 'Немецкий', 'french': 'Французский', 'spanish': 'Испанский', 'chinese': 'Китайский'}
     if len(filter['languages']) != 0:
-        users = {key: value for key, value in users.items() if set(value['languages']) == set(languagesDict[i] for i in filter['languages'])}
+        users = {key: value for key, value in users.items() if len(set(value['languages']).intersection(set(languagesDict[i] for i in filter['languages']))) != 0}
     else: pass
+
+    for i in users:
+        print(len(set(users[i]['languages']).intersection(set(languagesDict[i] for i in filter['languages']))) != 0)
 
 
     return users

a0f0b06774a0fa41ee5ab5fa7df19be65e544f5e
lubuntum <lubuntum@gmail.com>
2024-07-19 16:18:34 +0700

    Модуль для работы с таблицей users

--- auth/auth_user.py
+++ auth/auth_user.py
@@ -2,13 +2,13 @@ from flask import Flask, session
 from flask_bcrypt import Bcrypt
 import sqlite3
 
-from auth.user_table import getUser
+from auth.user_table import getUserByUsername
 from config import SECRET_KEY
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 def authUser(username, password):
-    user = getUser(username)
+    user = getUserByUsername(username)
     if user == None:
         return False
     if user and bcrypt.check_password_hash(user[2], password):

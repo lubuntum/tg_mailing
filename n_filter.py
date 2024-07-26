2030ac39af915c1bf4e911a89ee5c8e7e5a88148
lubuntum <lubuntum@gmail.com>
2024-07-26 15:59:13 +0700

    Отрефакторил фильтрацию по возрастам, прошу протестить и проверить, убрал постоянное использование регулярки

--- n_filter.py
+++ n_filter.py
@@ -21,7 +21,7 @@ def filterFunc(users, filter):
 
 
     return users
-
+'''
 def filterByAgeRange(users, filter, dataAgeKey, ageFromKey, ageToKey):
     if int(filter[ageFromKey]) < int(filter[ageToKey]):
         return {key: value for key, value in users.items()
@@ -38,3 +38,27 @@ def filterByAgeRange(users, filter, dataAgeKey, ageFromKey, ageToKey):
                 }
     else:
         return users
+'''
+def extract_ages(age_str):
+    return list(map(int, re.findall(r'\b\d+\b', age_str)))
+def is_within_range(user_ages, filter_from, filter_to):
+    if len(user_ages) == 2:
+        start, end = user_ages
+        return (start <= filter_from <= end) or (start <= filter_to <= end) or (filter_from <= start <= filter_to)
+    elif len(user_ages) == 1:
+        age = user_ages[0]
+        return filter_from <= age <= filter_to
+    return False
+
+
+def filterByAgeRange(users, filter, dataAgeKey, ageFromKey, ageToKey):
+    filter_from = int(filter[ageFromKey])
+    filter_to = int(filter[ageToKey])
+
+    if filter_from >= filter_to:
+        return users
+
+    return {
+        key: value for key, value in users.items()
+        if is_within_range(extract_ages(value[dataAgeKey]), filter_from, filter_to)
+    }
